a14048bf3145d3239344117909d634bd57c0861d
kiranmanam534 <kiran.manam.km@gmail.com>
2024-05-10 11:32:21 +0530

    done

--- App.tsx
+++ App.tsx
@@ -4,12 +4,14 @@ import Navigation from './src/components/Navigation'
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
 import MyComponent from './src/Test'
+import TestMapView from './TestMapView'
 
 const App = () => {
   return (
     <Provider store={store}>      
-    <Navigation/>
+    {/* <Navigation/> */}
     {/* <MyComponent/> */}
+    <TestMapView/>
     </Provider>
   )
 }
--- ios/wardcouncillor/AppDelegate.mm
+++ ios/wardcouncillor/AppDelegate.mm
@@ -2,6 +2,8 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+// #import <GoogleMaps/GoogleMaps.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -10,6 +12,8 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+  // [GMSServices provideAPIKey:@"AIzaSyCG4Tc5v-7PBF4JO-6NKx2aX0xDNCZ4BVM"];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
