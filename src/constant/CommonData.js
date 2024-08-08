b7d2af10295e6d8d7033e5fccbaabae2d7bee4a7
kiranmanam534 <kiran.manam.km@gmail.com>
2024-08-08 18:32:19 +0530

    BCX Done

--- src/constant/CommonData.js
+++ src/constant/CommonData.js
@@ -1,3 +1,3 @@
-//export const apiUrl='http://102.130.114.194:10001';
+export const apiUrl = 'http://102.130.114.194:10001';
 // export const apiUrl='http://192.168.196.21:5055';
-export const apiUrl='http://196.41.72.247:8083/WardsCoreApi';
+//export const apiUrl='http://196.41.72.247:8083/WardsCoreApi';
