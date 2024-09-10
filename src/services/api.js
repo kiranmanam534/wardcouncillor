a0792c33953a7df83a6520de0d622201ac0b7036
kiranmanam534 <kiran.manam.km@gmail.com>
2024-09-10 13:53:02 +0530

    apiURl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://192.168.1.8:5055';
-// const baseURL = 'https://myward.ekurhuleni.gov.za';
+// const baseURL = 'http://192.168.1.8:5055';
+const baseURL = 'https://myward.ekurhuleni.gov.za';
 // const baseURL='http://196.41.72.247:8083/WardsCoreApi'
 
 // Create an instance of Axios with custom configurations
